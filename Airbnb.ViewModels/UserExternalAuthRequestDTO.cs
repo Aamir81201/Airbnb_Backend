233c9db9bd5ba0e1f6d74d634b31ba29af83c80f
AamirDalal <mohammadamir.dalal@tatvasoft.com>
2024-02-08 18:04:36 +0530

    removed unused folders

--- Airbnb.ViewModels/UserExternalAuthRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Airbnb.ViewModels
-{
-    public class UserExternalAuthRequestDTO
-    {
-        public string TokenId { get; set; } = string.Empty;
-        public string Provider { get; set; }
-    }
-}
