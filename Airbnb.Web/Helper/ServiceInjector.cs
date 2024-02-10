a1ae82bbd4250f3c8e622bf3bc90f0915657d43d
Aamir Dalal <aamir81201@gmail.com>
2024-02-10 21:18:00 +0530

    bug fix

--- Airbnb.Web/Helper/ServiceInjector.cs
+++ Airbnb.Web/Helper/ServiceInjector.cs
@@ -1,5 +1,7 @@
 ﻿using Airbnb.Repository.Implementation;
 using Airbnb.Repository.Interface;
+using Airbnb.Service.Implementation;
+using Airbnb.Service.Interface;
 
 namespace Airbnb.Web.Helper
 {
@@ -7,8 +9,10 @@ namespace Airbnb.Web.Helper
     {
         public static void Inject(IServiceCollection services)
         {
-            services.AddScoped<IAirbnbRepository, AirbnbRepository>();   
+            services.AddScoped<IAirbnbRepository, AirbnbRepository>();
+            services.AddScoped<IAirbnbSerivce, AirbnbService>();
             services.AddScoped<IAirbnbCategoryRepository, AirbnbCategoryRepository>();
+            services.AddScoped<IAirbnbCategoryService, AirbnbCategoryService>();
             services.AddScoped<IAirbnbAmenityRepository, AirbnbAmenityRepository>();
             services.AddScoped<IAirbnbRepository, AirbnbRepository>();
         }
